21d891dd842988b19c6d66b00ecd135a867c4410
Phongsakorn Suttama <59281514+ipskn2dev@users.noreply.github.com>
2021-05-20 06:26:14 +0700

    Update sqlite.service.dart

--- lib/services/sqlite.service.dart
+++ lib/services/sqlite.service.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-void SqliteDatabase()  async {
+void sqliteDatabase() async {
   final database = openDatabase(
-    join(await getDatabasesPath(), client)
-  )
+    join(await getDatabasesPath(), 'user_db.db'),
+    onCreate: (db, version) {},
+  );
 }
